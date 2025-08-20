033c1aa7d17d58c559ea9cd14e921045f65dfd02
Kelvin Fon <100591000+mykefon@users.noreply.github.com>
2025-08-20 12:49:33 -0400

    Use thread-safe Random

--- WeatherForecast.Domain/WeatherService.cs
+++ WeatherForecast.Domain/WeatherService.cs
@@ -2,10 +2,8 @@
 
 public class WeatherService : IWeatherService
 {
-    private readonly Random _random = new();
-
     public int GetAverageTemperature(string city, DateTimeOffset dateTimeOffset)
     {
-        return _random.Next(-10, 40);
+        return Random.Shared.Next(-10, 40);
     }
 }
